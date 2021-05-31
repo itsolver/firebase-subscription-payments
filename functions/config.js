0799d2e7a04600189007155ea7d2407e0177343d
itsolver <angus@itsolver.net>
2021-05-31 13:17:26 +1000

    remove test keys from git

--- functions/config.js
+++
@@ -1,6 +0,0 @@
-export default {
-    stripeSecretKey: "rk_test_51ISccSIRnn4YZ0ErmV6TEoFmq46fRQKcnwFdrj1hA06MylW2TaF8zJMtub1JSm0MKTuVHvB3XpMBqCoI7eeUIUfB00LVTmOdkh",
-    stripeWebhookSecret: "whsec_P1dPU53zUWYSsDcXgJ4iZ4NQEK3FOBRP",
-    productsCollectionPath: process.env.PRODUCTS_COLLECTION,
-    customersCollectionPath: process.env.CUSTOMERS_COLLECTION,
-  };
